90f4ea9251e1a4fb274eb3919a584ef64c774b10
Adepoju Opeyemi <opepoju@gmail.com>
2025-09-19 11:38:52 +0100

    fix (refactor): added p tag

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -1,7 +1,8 @@
+'use client'
 export default function Footer() {
     return (
         <footer className="bg-gray-900 py-8 text-center text-gray-400">
-            © {new Date().getFullYear()} AttendEase. Built for Students. 💡
+            <p> © {new Date().getFullYear()} AttendEase. Built for Students. 💡</p>
         </footer>
     )
 }
